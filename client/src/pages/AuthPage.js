31f3b05459a3abd5ce8e9c21ef1088801329f6ff
Vsevolod Berdutin <vesevolodberdutin@gmail.com>
2021-06-10 12:09:15 +0300

    first commit

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+
+export const AuthPage = () => {
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
+
+  const changeHandler = event => {
+    setForm({...form, [event.target.name]: event.target.value})
+  }
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Shorten the link</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Autorisation</span>
+            <div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Input email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                  />
+                <label htmlFor="first_name">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Input password"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                  />
+                <label htmlFor="email">Password</label>
+              </div>
+
+            </div>
+          </div>
+          <div className="card-action">
+            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>
+              Sign in
+            </button>
+            <button className="btn grey lighten-1 black-text">Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ routes/auth.routes.js
@@ -0,0 +1,93 @@
+const { Router } = require('express')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const { check, validationResult } = require('express-validatior')
+const User = require('../models/User')
+const router = Router()
+
+// /api/auth/register
+router.post(
+  '/register',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check(
+      'password',
+      'The password must be of minimum length 6 characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (rec, res) => {
+    try {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message: 'Registrations data is incorrect'
+            })
+        }
+
+      const { email, password } = req.body
+
+      const candidate = await User.findOne({ email })
+
+      if (candidate) {
+        return res.status(400).json({ message: 'Such user already exists' })
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12)
+      const user = new User({ email, password: hashedPassword })
+    } catch (e) {
+      res.status(500).json({ message: 'Something gone wrong' })
+    }
+  }
+)
+
+// /api/auth/login
+router.post(
+    '/login',
+    [
+        check('email', 'Enter a valid email').normalizeEmail().isEmail(),
+        check('password','Enter password'). exist()
+    ],
+    async (rec, res) => {
+        try {
+
+            const errors = validationResult(req)
+    
+            if (!errors.isEmpty()){
+                return res.status(400).json({
+                    errors:errors.array(),
+                    message: 'Login data is incorrect'
+                })
+            }
+
+            const {email,password} = req.body
+
+            const user = await User.findOne({email})
+
+            if (!user){
+                return res.status(400).json({message:'User is not found'})
+            }
+            
+            const isMatch = await bcrypt.compare(password,user.password)
+            
+            if (!isMatch){
+                return res.status(400).json({message:'Password is incorrect, try again'})
+            }
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            res.json({token, userId: user.id})
+
+
+        } catch (e) {
+          res.status(500).json({ message: 'Something gone wrong' })
+        }   
+    })
+module.exports = router
